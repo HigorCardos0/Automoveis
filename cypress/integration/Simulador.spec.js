eb0c5ca3920d05b50dae05ccd4b31f62bbf7db2d
brohi <brohigor@gmail.com>
2024-05-13 16:41:54 -0300

    Cypress atualizado

--- cypress/integration/Simulador.spec.js
+++
@@ -1,9 +0,0 @@
-
-describe('Simulador basico V1',()=>{
-    beforeEach(()=>{
-    cy.visit('./UI/index.html');
-  });
-    it('verifica ',()=> {
-      cy.get(':nth-child(5) > :nth-child(1)').should('be.visible');
-      }); 
-});
